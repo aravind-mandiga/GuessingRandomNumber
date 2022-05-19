e64f097d4b7325bb031637206edb7243d3c97bf9
mandigaaravind <mandigaaravind@gmail.com>
2022-05-19 11:54:25 +0530

    guess

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+fn main() {
+    println!("guess the number!");
+    let secret_number:u32 = rand::thread_rng().gen_range(1..101);
+    
+    loop{
+    println!("please input your guess.");
+    let mut guess = String::new();
+
+    io::stdin().read_line(& mut guess)
+    .expect("failed to read line");
+
+    let guess:u32=match guess.trim().parse(){
+        Result::Ok(num)=>num,
+        Result::Err(_)=>continue,
+    };
+
+
+    println!("you guessed {}",guess);
+    match guess.cmp(&secret_number) {
+        Ordering::Less=>println!("too small!"),
+        Ordering::Greater=>println!("Too big!"),
+        Ordering::Equal=>{
+            println!("you win!");
+            break;
+        }
+    }
+}
+}
